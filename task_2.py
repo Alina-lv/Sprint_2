8bd15f1725d3235dbee9f55a7d50249d47b87495
Alina Lvova <lvova.ad@gmail.com>
2025-04-11 00:00:15 +0300

    В классах Comedy и Drama метод add_movie() теперь вызывает родительский метод через super() вместо повторный реализации. Добавлен форматированный вывод фильмов - теперь они отображаются как строка, разделенная запятыми  (без квадратный скоробок)

--- task_2.py
+++ task_2.py
@@ -5,22 +5,23 @@ class Movies:
     def add_movie(self, movie):
         self.movies.append(movie)
 
-
 class Comedy(Movies):
     def add_movie(self, movie):
-        self.movies.append(movie)
-        return f"Комедии: {self.movies}"
-
+        super().add_movie(movie)
+        movies_list = ', '.join(self.movies)
+        return f"Комедии: {movies_list}"
 
 class Drama(Movies):
     def add_movie(self, movie):
-        self.movies.append(movie)
-        return f"Драмы: {self.movies}"
-
+        super().add_movie(movie)
+        movies_list = ', '.join(self.movies)
+        return f"Драмы: {movies_list}"
 
 # Тестирование
-comedy_movie = Comedy()
-print(comedy_movie.add_movie('Большой куш'))
+comedy = Comedy()
+print(comedy.add_movie('Большой куш'))
+print(comedy.add_movie('Маска'))
 
-drama_movie = Drama()
-print(drama_movie.add_movie('Оружейный барон'))
+drama = Drama()
+print(drama.add_movie('Оружейный барон'))
+print(drama.add_movie('Зелёная миля'))
